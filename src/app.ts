51e04e3e73869ccc2ccd7840c3520b2a21d96932
piyush <piyushujgaokar544@gmail.com>
2026-04-06 23:46:04 +0530

    implement agents and the models to the preview the solutions of the different models

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import runGraph from './ai/graph.ai.js'
+
+const app=express();
+
+app.get('/',async (req,res)=>{
+        const result= await runGraph("write a code for factorial function in js")
+        res.json(result)
+})
+
+
+export default app;
